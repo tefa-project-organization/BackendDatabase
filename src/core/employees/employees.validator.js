2f5f76dd11905c2b89b46984ded39a0291d30057
Virazkok <vickyhernawan@yahoo.com>
2025-12-19 18:39:14 +0700

    employees

---
+++ src/core/employees/employees.validator.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export const employeesValidator = {
+  create: Joi.object({
+    // no-data
+  }),
+  update: Joi.object({
+    // no-data
+  }),
+};
+
+export default employeesValidator;
