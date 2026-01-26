0359d8e4ab1303c359c1ddf3c1faa7e5a83a4e8c
Virazkok <vickyhernawan@yahoo.com>
2026-01-26 07:51:40 +0700

    yahahah

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ app.use(cookieParser());
 ========================== */
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5174",
   "https://frontend-anda.vercel.app", // GANTI sesuai frontend prod
 ];
 
